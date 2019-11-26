deefe0b8336e96b10e90bf7f7744d4c54af65c72
MiYazJE <ruben.saizserrano@gmail.com>
2019-11-26 18:08:59 +0100

    Atacks added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 
 async function loadPokemons() {
+    console.log(API_POKEMON)
     const response = await fetch(API_POKEMON);
     const json     = await response.json();
+    API_POKEMON    = json.next;
     pokemons       = json.results;
 }
 
@@ -14,17 +16,25 @@ function rellenarInformacion() {
         let src           = jsonPokemon.sprites['front_default'];
 
         // Recoger descripcion del pokemon a partir de su id
-        const responseDescription = await fetch(API_POKEMON_DESCRIPTION + jsonPokemon.id);
+        const responseDescription = await fetch(DESCRIPTION + ((jsonPokemon.id % 30) + 1));
         const jsonDescription     = await responseDescription.json();
         let description           = jsonDescription.descriptions[1].description;
 
+        // Recoger los movimientos
+        let movesContent = '';
+        for (let i = 0; i < 4; i++) {
+            movesContent += `
+                <div class="move">${jsonPokemon.moves[i].move.name}</div>
+            `;
+        }
+
         // Crear la estructura html con la información del pokemon y agregarla al body
-        let pokemonBox = createPokemonBox(pokemon.name, src, description, jsonPokemon.id);
+        let pokemonBox = createPokemonBox(pokemon.name, src, description, jsonPokemon.id, movesContent);
         body.innerHTML += pokemonBox;
     })
 }
 
-function createPokemonBox(nombre, src, description, id) {
+function createPokemonBox(nombre, src, description, id, moves) {
     return `
         <div class="wrapPokemon">
 
@@ -37,11 +47,14 @@ function createPokemonBox(nombre, src, description, id) {
             <div class="contenido">
                 <div class="imagen">
                     <img src="${src}" class="foto">
+                    <div class="wrapDescription">
+                        <p class="descripcion">${description}</p>
+                    </div>
                 </div>
                 <div class="info">
-                    <div class="wrapDescription">
-                        <h3>DESCRIPTION</h3>
-                        <p class="descripcion">${description}</p>
+                    <div class="movesWrap">
+                        <h3 class="move">ATACKS</h3>
+                        ${moves}
                     </div>
                 </div>
             </div>
@@ -50,13 +63,21 @@ function createPokemonBox(nombre, src, description, id) {
     `;
 }
 
-const API_POKEMON = 'https://pokeapi.co/api/v2/pokemon';
-const API_POKEMON_DESCRIPTION = 'https://pokeapi.co/api/v2/characteristic/';
+let API_POKEMON = 'https://pokeapi.co/api/v2/pokemon';
+const DESCRIPTION = 'https://pokeapi.co/api/v2/characteristic/';
 
 const body = document.querySelector('body');
 let pokemons;
 
+document.querySelector('.btnVerMas').onclick = () => {
+    loadPokemons()
+        .then(rellenarInformacion)
+        .catch(err => err)
+}
+
 // Cargar objetos "pokemon"
-loadPokemons()
-    .then(rellenarInformacion)
-    .catch(err => err);
+setInterval( () => {
+    loadPokemons()
+        .then(rellenarInformacion)
+        .catch(err => err)
+}, 5000);
